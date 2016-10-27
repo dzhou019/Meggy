3e37550d97685ef71c29c159e983b99e5f92d3e5
dzhou019 <dzhou19@punahou.edu>
2016-10-26 14:53:36 -1000

    made snake long line
    I made the snake a bunch of points and move together to touch an apple
    and light the top light every time an apple is touched

--- Snake/Snake.ino
+++ Snake/Snake.ino
@@ -42,7 +42,9 @@ int x=0;                    // Required code, line 1 of 2.
 int y=0;
 int xapple=random(8);
 int yapple=random(8);  
+boolean gotapple = true;
 int binary= 0;
+int speed =250;
      
                                 
 struct Point
@@ -53,15 +55,20 @@ struct Point
 
 Point p1 = {2,4};
 Point p2 = {3,4};
-Point p3 = {3,4};
-Point p4 = {3,4};
+Point p3 = {4,4};
+Point p4 = {5,4};
 Point snakeArray[64] = {p1,p2,p3,p4};
-int marker = 4;// Index of the first empty segement of array
-int direction =0;
+int marker = 4;                // Index of the first empty segement of array
+int direction = 0;
+
+void drawSnake();
+
 void setup()                    // run once, when the sketch starts
 {
   MeggyJrSimpleSetup();      // Required code, line 2 of 2.
 }
+
+
 
 void loop()                     // run over and over again
 {
@@ -69,60 +76,68 @@ void loop()                     // run over and over again
   DrawPx(xapple,yapple,Red);
   //Have eaten apple
   if (ReadPx(snakeArray[0].x,snakeArray[0].y)==Red)
+  {
+    xapple = random(8);         // Roll new values for apple
+    yapple = random(8);  
+    Tone_Start(18182,50);
+    if (binary < 128)
     {
-      xapple = random(8);         // Roll new values for apple
-      yapple = random(8);  
-      Tone_Start(18182,50);
-      if (binary <128)
-       binary = binary * 2 + 1;
-       else binary = 0;
-     }
+      binary = binary * 2 + 1;
+    }
+    else   
+    {
+      binary = 0; 
+        
+    }
+  }
+  
   drawSnake();
   SetAuxLEDs(binary);
   DisplaySlate();                  // Write the drawing to the screen.
-  delay(250);                     // waits for a second 
-  CheckButtonsPress();                              
+  delay(speed);                     // waits for a second 
+  CheckButtonsDown();                              
   ClearSlate();                 // Erase drawing
   
   if (Button_Right)
-    {
-     direction=90;
-    }
+  {
+    direction=90;
+  }
   if (Button_Up)
-    {
-      direction=0;
-    }
+  {
+     direction=0;
+  }
   if (Button_Down)
-    {
-      direction=180;
-    }
+  {
+     direction=180;
+  }
   if (Button_Left)
-    {
-      direction=270;
-    }
-  
-                
+  {
+     direction=270;
+  }
 }
+
+
 // Checks the direction and updates the x or y value
 void updateSnake()
 {
-  //Move body
-  for (int = marker -1; i > 0; i--)
+                                      //Move body
+  
+  for (int i = marker - 1; i > 0; i--)
   {
     // Copy values at i-1 into i
-    snakeArray[i]= snakeArray[i-1];
+     snakeArray[i]= snakeArray[i-1];
   }
   // Move head
-  if (direction ==0)
-    {
+  if (direction == 0)
+  {
     // Updates y
     snakeArray[0].y = snakeArray[0].y+1;
     // Corrects for out of bounds
     if (snakeArray[0].y >7)
-    {
+     {
       snakeArray[0].y=0;
-  }
- }
+     }
+    }
  if (direction ==90)
   {
     // Updates x
@@ -131,8 +146,8 @@ void updateSnake()
     if (snakeArray[0].x >7)
     {
       snakeArray[0].x=0;
+    }
   }
- }
  if (direction ==180)
   {
     // Updates y
@@ -153,14 +168,17 @@ void updateSnake()
      {
       snakeArray[0].x=7;
      }
-  }
- 
+   }
 }
+
+
 void drawSnake()
 {
   // Iterate through entire array and draw each segement
-  for (int = 0; i < marker; i++)
-  DrawPx(snakeArray[i].x, snakeArray[i].y, Blue);      // Draw the snake.
+    for (int i = 0; i < marker; i++)
+    {  
+      DrawPx(snakeArray[i].x, snakeArray[i].y, Blue); // Draw the snake.                                       
+    }
 }
 
 
